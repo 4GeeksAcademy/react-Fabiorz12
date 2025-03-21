ccadb837457077650539f0ffb525bfb2f89fad85
Fabio Rodríguez <fabioaro10@gmail.com>
2025-03-21 17:50:24 +0000

    proyecto terminado

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -4,20 +4,23 @@ const Navbar = () => {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="#">Mi Sitio</a>
+          <a className="navbar-brand" href="#">Start Bootstrap</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
+          <div className="collapse navbar-collapse " id="navbarNav">
+            <ul className="navbar-nav ms-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Inicio <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Acerca de</a>
+                <a className="nav-link" href="#">About</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contacto</a>
+                <a className="nav-link" href="#">Services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
               </li>
             </ul>
           </div>
