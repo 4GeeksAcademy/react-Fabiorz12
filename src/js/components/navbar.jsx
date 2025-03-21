697a1e6224de26c3339e15bbb7135382993f87ec
Fabio Rodríguez <fabioaro10@gmail.com>
2025-03-21 18:02:55 +0000

    cambio de color de la barra

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
         <div className="container">
           <a className="navbar-brand" href="#">Start Bootstrap</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
